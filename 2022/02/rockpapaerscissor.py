90128bea789b77c836775d7006cd332595cb5730
Punktigel <punktigel@gmail.com>
2022-12-03 21:32:11 +0100

    Cleanup

--- 2022/02/rockpapaerscissor.py
+++ 2022/02/rockpapaerscissor.py
@@ -1,25 +1,21 @@
 
-#                 Rock        Paper     Scissors
+#                Rock(1p)   Paper(2p)  Scissors(3p)
 list_options = [('A','X'), ('B', 'Y'), ('C', 'Z')]
 
-def get_number(option):
+def get_index(option):
     for i in range(len(list_options)):
         if list_options[i][0] == option or list_options[i][1] == option:
             return i 
 
 
-def get_points(op, my):
-    indx_op = get_number(op)
-    indx_me = get_number(my)
+def get_points(indx_op, indx_me):
+    if (indx_op + 1) % 3 == indx_me: # Win
+        return 6 + indx_me + 1
 
-    points_round = indx_me + 1 #points from symbol
-    if indx_op == indx_me: # same element - draw
-        points_round += 3
+    elif indx_op == indx_me: # Draw
+        return 3 + indx_me + 1
 
-    elif (indx_op + 1) % 3 == indx_me: # win
-        points_round += 6
-    
-    return points_round
+    return indx_me + 1 # Lose
 
 
 def get_max_points(file):
@@ -29,23 +25,21 @@ def get_max_points(file):
         for line in f:
             options = line.replace("\n", '').split(' ')
 
-            sum_1 += get_points(options[0], options[1])
-            sum_2 += change_option(options[0], options[1])
+            sum_1 += get_points(get_index(options[0]), get_index(options[1]))
+            sum_2 += change_option(get_index(options[0]), options[1])
 
     print(f"Solution: \na): {sum_1}\nb): {sum_2}")
 
             
 # Part 2:
-def change_option(op_option, my_option):
-    indx = get_number(op_option)
-    if my_option == "X": # Lose
-        return (indx -1) % 3 +1  
+def change_option(indx, my_option):
+    if my_option == "Z": # Win
+        return 6 + (indx + 1) % 3 + 1
 
     elif my_option == "Y": # Draw
         return 3 + indx + 1
 
-    else: # Win
-        return 6 + (indx + 1) % 3 + 1
+    return (indx -1) % 3 + 1 # Lose
 
 
 if __name__ == "__main__":
@@ -53,10 +47,10 @@ if __name__ == "__main__":
 
 """
 Part 1:
-            OP  |  You      | Points    |
-Rock:       A   |   X       |   1p      |   +0p     | Lose
-Paper:      B   |   Y       |   2p      |   +3p     | Draw
-Scissor:    C   |   Z       |   3p      |   +6p     | Win
+            Fst |  Snd  | Points    |
+Rock:       A   |   X   |   1p      |   +0p     | Lose
+Paper:      B   |   Y   |   2p      |   +3p     | Draw
+Scissor:    C   |   Z   |   3p      |   +6p     | Win
 
 
 Part 2:
