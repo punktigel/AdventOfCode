526f4efe86e17fda688275230af57524355ee673
Punktigel <punktigel@gmail.com>
2022-12-03 21:50:55 +0100

    Cleanup

--- 2022/01/calories.py
+++ 2022/01/calories.py
@@ -1,20 +1,15 @@
-def change_array(arr, num):
-    indx = 0
-    for i in range(len(arr)):
-        if arr[i] < arr[indx]:
-            indx = i
-    if num > arr[indx]:
-        arr[indx] = num
+def set_top_three(arr, num):
+    if num > min(arr):
+        arr[arr.index(min(arr))] = num
  
-
 def get_max(file):
     counter = 0
-    top_three = [0 for i in range(3)] #initialize array [0,0,0]
+    top_three = [0 for i in range(3)]
 
     with open(file, 'r') as f:
         for line in f:
-            if line.replace(" ", "") == "\n":
-                change_array(top_three, counter)
+            if line.replace(' ', '') == "\n":
+                set_top_three(top_three, counter)
                 counter = 0
 
             else:
